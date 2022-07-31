c15b6906cdbfd7c958145e001994ea73a7f6dd7c
Sander Paekivi <49115856+SanderPaekivi@users.noreply.github.com>
2022-07-31 21:46:18 +0300

    fixed app.py to work with gunicorn. Edited Graph and prediction text to be more cohesive and informative

--- app.py
+++ app.py
@@ -35,84 +35,80 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
 
-
-app = Flask(__name__)
+working_dir = '/root/sample-flask/'
+#app = Flask(__name__)
 #run_with_ngrok(app)
 
-if __name__ == '__main__':
+def __ma_model(n_points, noise_std = 1, noise_alpha = 2, params = []):
+    ma_order = len(params)
+    if noise_alpha == 2:
+        noise = norm.rvs(scale=noise_std, size=(n_points + ma_order))
+    else:
+        noise = levy_stable.rvs(
+            noise_alpha, 0, scale=noise_std, size=(n_points + ma_order)
+        )
+
+    if ma_order == 0:
+        return noise
+    ma_coeffs = np.append([1], params)
+    ma_series = np.zeros(n_points)
+    for idx in range(ma_order, n_points + ma_order):
+        take_idx = np.arange(idx, idx - ma_order - 1, -1).astype(int)
+        ma_series[idx - ma_order] = np.dot(ma_coeffs, noise[take_idx])
+    return ma_series[ma_order:]
+
+def __frac_diff(x, d):
+
+    def next_pow2(n):
+        return (n - 1).bit_length()
+
+    n_points = len(x)
+    fft_len = 2 ** next_pow2(2 * n_points - 1)
+    prod_ids = np.arange(1, n_points)
+    frac_diff_coefs = np.append([1], np.cumprod((prod_ids - d - 1) / prod_ids))
+    dx = ifft(fft(x, fft_len) * fft(frac_diff_coefs, fft_len))
+    return np.real(dx[0:n_points])
+
+def __arma_model(noise, params = []):
+
+    ar_order = len(params)
+    if ar_order == 0:
+        return noise
+    n_points = len(noise)
+    arma_series = np.zeros(n_points + ar_order)
+    for idx in np.arange(ar_order, len(arma_series)):
+        take_idx = np.arange(idx - 1, idx - ar_order - 1, -1).astype(int)
+        arma_series[idx] = np.dot(params, arma_series[take_idx]) + noise[idx - ar_order]
+    return arma_series[ar_order:]
+
+
+class LSTMnetwork(nn.Module):
+    def __init__(self,input_size=1,hidden_size=100,output_size=1):
+        super().__init__()
+        self.hidden_size = hidden_size
+
+        # Add an LSTM layer:
+        self.lstm = nn.LSTM(input_size,hidden_size)
+
+        # Add a fully-connected layer:
+        self.linear = nn.Linear(hidden_size,output_size)
+
+        # Initialize h0 and c0:
+        self.hidden = (torch.zeros(1,1,self.hidden_size),
+                        torch.zeros(1,1,self.hidden_size))
+
+    def forward(self,seq):
+        lstm_out, self.hidden = self.lstm(
+            seq.view(len(seq),1,-1), self.hidden)
+        #return lstm_out
+        pred = self.linear(lstm_out.view(len(seq),-1))
+        #pred = self.linear(lstm_out[-50:].view(50,-1))
+        return pred[-1]
     
-    def __ma_model(n_points, noise_std = 1, noise_alpha = 2, params = []):
-        ma_order = len(params)
-        if noise_alpha == 2:
-            noise = norm.rvs(scale=noise_std, size=(n_points + ma_order))
-        else:
-            noise = levy_stable.rvs(
-                noise_alpha, 0, scale=noise_std, size=(n_points + ma_order)
-            )
-
-        if ma_order == 0:
-            return noise
-        ma_coeffs = np.append([1], params)
-        ma_series = np.zeros(n_points)
-        for idx in range(ma_order, n_points + ma_order):
-            take_idx = np.arange(idx, idx - ma_order - 1, -1).astype(int)
-            ma_series[idx - ma_order] = np.dot(ma_coeffs, noise[take_idx])
-        return ma_series[ma_order:]
-
-    def __frac_diff(x, d):
-
-        def next_pow2(n):
-            return (n - 1).bit_length()
-
-        n_points = len(x)
-        fft_len = 2 ** next_pow2(2 * n_points - 1)
-        prod_ids = np.arange(1, n_points)
-        frac_diff_coefs = np.append([1], np.cumprod((prod_ids - d - 1) / prod_ids))
-        dx = ifft(fft(x, fft_len) * fft(frac_diff_coefs, fft_len))
-        return np.real(dx[0:n_points])
-
-    def __arma_model(noise, params = []):
-
-        ar_order = len(params)
-        if ar_order == 0:
-            return noise
-        n_points = len(noise)
-        arma_series = np.zeros(n_points + ar_order)
-        for idx in np.arange(ar_order, len(arma_series)):
-            take_idx = np.arange(idx - 1, idx - ar_order - 1, -1).astype(int)
-            arma_series[idx] = np.dot(params, arma_series[take_idx]) + noise[idx - ar_order]
-        return arma_series[ar_order:]
-
-
-    class LSTMnetwork(nn.Module):
-        def __init__(self,input_size=1,hidden_size=100,output_size=1):
-            super().__init__()
-            self.hidden_size = hidden_size
-
-            # Add an LSTM layer:
-            self.lstm = nn.LSTM(input_size,hidden_size)
-
-            # Add a fully-connected layer:
-            self.linear = nn.Linear(hidden_size,output_size)
-
-            # Initialize h0 and c0:
-            self.hidden = (torch.zeros(1,1,self.hidden_size),
-                           torch.zeros(1,1,self.hidden_size))
-
-        def forward(self,seq):
-            lstm_out, self.hidden = self.lstm(
-                seq.view(len(seq),1,-1), self.hidden)
-            #return lstm_out
-            pred = self.linear(lstm_out.view(len(seq),-1))
-            #pred = self.linear(lstm_out[-50:].view(50,-1))
-            return pred[-1]
-        
-    model = LSTMnetwork()
-    model = torch.load('LSTM100x1_model.pkl')
-    
-     app.run(host='0.0.0.0')
+model = LSTMnetwork()
+model.load_state_dict(torch.load(working_dir + 'LSTM100x1_model_state.pkl'))
+model.eval()
  
-
 @app.route('/', methods=['GET'])
 def home():
     
@@ -121,13 +117,9 @@ def home():
 
 @app.route('/', methods=['POST'])
 def predict():
-    #alpha_to_gen = list(request.form.values())
-    #print(alpha_to_gen)
-    #alpha_to_gen = float(alpha_to_gen[0])
-    #ts_len = float(alpha_to_hen[1])
 
     inputs = request.form.to_dict()
-    print(inputs)
+    #print(inputs)
     alpha_to_gen = float(inputs['Alpha'])
     ts_len = int(inputs['tsl'])
 
@@ -150,12 +142,18 @@ def predict():
 
     img = BytesIO()
     plt.plot(data)
+    plt.grid()
+    plt.title(f'ARFIMA time-series generated with given value for alpha: {alpha_to_gen}');
+    plt.ylabel('ARFIMA(t)');
+    plt.xlabel('time-steps');
     plt.savefig(img,format='png')
     plt.close()
     img.seek(0)
     plot_url = base64.b64encode(img.getvalue()).decode('utf8')
 
 
-    return render_template('index.html', prediction_text='The Hurst parameter of generated data was estimated by the LSTM to be {}'.format(prediction), 
+    return render_template('index.html', prediction_text=f'You generated {ts_len} steps of an ARFIMA time-series with alpha {alpha_to_gen}. The LSTM predicts the alpha to be {prediction}. Difference is {round(abs(prediction-alpha_to_gen),3)}. How did I do?', 
                            figure_to_print = plot_url)
 
+if __name__ == '__main__':    
+     app.run(host='0.0.0.0')
