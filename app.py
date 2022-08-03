d00f2271b03cbe35fb7465e2ac86541f7e0bff9d
Sander Paekivi <49115856+SanderPaekivi@users.noreply.github.com>
2022-08-04 01:31:06 +0300

    reg line to scatter

--- app.py
+++ app.py
@@ -144,6 +144,8 @@ def predict():
         img = BytesIO()
         plt.scatter(alphas, LSTMalphas, color ='c')
         plt.grid()
+        lr1, lr2 = np.polyfit(alphas, LSTMalphasalphas, LSTMalphas, y, 1)
+        plt.plot(alphas, lr1*alphas+lr2)
         plt.title(f'ARFIMA alpha vs LSTM estimate.');
         plt.ylabel('LSTM alpha');
         plt.xlabel('Real alpha');
