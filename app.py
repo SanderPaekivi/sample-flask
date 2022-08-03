a790cb5622151196d481e16fa64378ba766b50d9
Sander Paekivi <49115856+SanderPaekivi@users.noreply.github.com>
2022-08-04 02:01:03 +0300

    made reg line black

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ def predict():
         plt.grid()
         coefs = np.polyfit(alphas, LSTMalphas, 1)
         lr1, lr2 = coefs
-        plt.plot(alphas, [lr1*i for i in list(alphas)]+lr2)
+        plt.plot(alphas, [lr1*i for i in list(alphas)]+lr2, color='black')
         yfit = np.polyval(coefs,alphas)
         mean_abs_error = np.mean( [abs(LSTMalphas[i]-yfit[i]) for i in range(len(yfit))] )
         #Need to check for a better way to give position, but this works for now. 
