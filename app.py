e5b1fc6685d673d0abd3583947d78b0c63991ed1
Sander Paekivi <49115856+SanderPaekivi@users.noreply.github.com>
2022-08-04 10:29:04 +0300

    revert

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ def predict():
 
     elif request.form["submit_button"] == 'give summary':
 
-        model_struct_text = 'PyTorch model: \n'+repr(model)
+        model_struct_text = 'PyTorch model: \n '+repr(model)
         model_struct_text = model_struct_text.split('\n')
         #model_struct_text = ['<br>'+item for item in model_struct_text]
         model_struct_text = model_struct_text.replace('\n', '<br>')
