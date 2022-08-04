1802ebe69271a87d99f6909fdaeebdd7289e6d18
Sander Paekivi <49115856+SanderPaekivi@users.noreply.github.com>
2022-08-04 10:29:42 +0300

    hmm

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ def predict():
         model_struct_text = 'PyTorch model: \n '+repr(model)
         model_struct_text = model_struct_text.split('\n')
         #model_struct_text = ['<br>'+item for item in model_struct_text]
-        model_struct_text = model_struct_text.replace('\n', '<br>')
+        #model_struct_text = model_struct_text.replace('\n', '<br>')
 
         return render_template('index.html', prediction_text=model_struct_text)
 
