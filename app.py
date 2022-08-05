24b13febfd55ff5b01247e0a82d6c2f1f35b0453
Sander Paekivi <49115856+SanderPaekivi@users.noreply.github.com>
2022-08-05 14:02:04 +0300

    get lstm?

--- app.py
+++ app.py
@@ -126,6 +126,9 @@ def goto_lstm():
     if request.form['submit_button'] == 'goto_lstm_button':
         return render_template('index.html', predict_text='')
 
+@app.route('/lstm', methods=['GET'])
+def lstm_home():
+    return render_template('index.html', predict_text='')
 
 @app.route('/lstm', methods=['POST'])
 def predict():
